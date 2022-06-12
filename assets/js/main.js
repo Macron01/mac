9cc238120835d500b879ec757756fc6701ca9b6a
Macron01 <106742906+Macron01@users.noreply.github.com>
2022-06-12 20:15:02 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -241,6 +241,37 @@
       once: true,
       mirror: false
     })
+     /**
+   * Animation on scroll
+   */
+    const OPTS = {
+  fill:           'none',
+  radius:         15,
+  strokeWidth:    { 50 : 0 },
+  scale:          { 0: 1 },
+  angle:          { 'rand(-35, -70)': 0 },
+  duration:       500,
+  left: 0,        top: 0,
+  easing: 'cubic.out'
+};
+
+const circle1 = new mojs.Shape({
+  ...OPTS,
+  stroke:         '#90ee90',
+});
+
+document.addEventListener( 'click', function (e) {
+  
+   circle1
+    .tune({ x: e.pageX, y: e.pageY  })
+    .replay();
+  
+  circle2
+    .tune({ x: e.pageX, y: e.pageY  })
+    .replay();
+  
+});
+
   });
 
 })()
